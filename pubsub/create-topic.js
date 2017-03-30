59fe7a15aa0a95eaa063feae56f67d9efb88502a
feroult <feroult@gmail.com>
2017-03-30 18:04:38 -0300

    pubsub

--- pubsub/create-topic.js
+++ pubsub/create-topic.js
@@ -1,19 +1,15 @@
 #!/usr/bin/env node
 
-const pubsubClient = require('./pubsub');
+const pubsub = require('./pubsub');
 
 const argv = require('yargs')
-    .usage('Usage: $0 -t [topic]')
     .demandOption('t')
     .argv;
 
 const topic = argv.t;
 
-pubsubClient.createTopic(topic)
+pubsub.createTopic(topic)
     .then((results) => {
         const topic = results[0];
         console.log(`Topic ${topic.name} created.`);
-    })
-    .catch(err => {
-        console.log('err', err);
     });
---
+++ pubsub/publish.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+const pubsub = require('./pubsub');
+
+const argv = require('yargs')
+    .demandOption('t')
+    .demandOption('d')
+    .argv;
+
+const topicName = argv.t;
+const data = argv.d;
+
+const topic = pubsub.topic(topicName);
+
+return topic.publish(data)
+    .then((results) => {
+        const messageIds = results[0];
+        console.log(`Message ${messageIds[0]} published.`);
+        return messageIds;
+    });
