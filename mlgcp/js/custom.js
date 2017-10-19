abf284fd9f769dee997d4795b50910def4457755
feroult <feroult@gmail.com>
2017-10-19 10:16:42 -0200

    slides

--- mlgcp/js/custom.js
+++ mlgcp/js/custom.js
@@ -8,6 +8,11 @@ Reveal.configure({
     transition: 'slide'
 });
 
+head.js("https://cdnjs.cloudflare.com/ajax/libs/lodash.js/2.4.1/lodash.min.js");
+head.js("https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js");
+head.js("https://www.gstatic.com/firebasejs/4.5.0/firebase.js");
+
+
 head.ready("jquery.min.js", function () {
     const $ = jQuery;
     $('<div id="subtitle" />').appendTo('body');
@@ -15,7 +20,12 @@ head.ready("jquery.min.js", function () {
 
 head.ready("firebase.js", function () {
     const $ = jQuery;
+    firebaseInit();
+    handleSubtitles($);
+    handleCommands($);
+});
 
+function firebaseInit() {
     var config = {
         apiKey: "AIzaSyDnVN1AS06i2I3RB-gsmWd-2feIZs2AS4A",
         authDomain: "serverlezz.firebaseapp.com",
@@ -25,7 +35,9 @@ head.ready("firebase.js", function () {
         messagingSenderId: "439862729073"
     };
     firebase.initializeApp(config);
+}
 
+function handleSubtitles($) {
     const db = firebase.database();
     const ref = db.ref('subtitles');
 
@@ -41,10 +53,55 @@ head.ready("firebase.js", function () {
     ref.once('value', function (snap) {
         init = true;
     });
-});
+}
 
-head.js("https://cdnjs.cloudflare.com/ajax/libs/lodash.js/2.4.1/lodash.min.js");
-head.js("https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js");
-head.js("https://www.gstatic.com/firebasejs/4.5.0/firebase.js");
+var enableCommands = false;
 
+function handleCommands($) {
+    const db = firebase.database();
+    const ref = db.ref('commands');
+
+    var init = false;
+    ref.on('child_added', function (snap) {
+        if (init) {
+            const cmd = snap.val();
+            console.log('cmd', cmd);
+            if (cmd.action === "enable") {
+                enableCommands = true;
+                return;
+            }
+            if (cmd.action === "disable") {
+                enableCommands = false;
+                return;
+            }
+            if (!enableCommands) {
+                return;
+            }
+            if (cmd.action === "next") {
+                cmdNext(cmd);
+                return;
+            }
+            if (cmd.action === "previous") {
+                cmdPrevious(cmd);
+                return;
+            }
+        }
+    });
+
+    ref.once('value', function (snap) {
+        init = true;
+    });
+}
+
+function cmdNext(cmd) {
+    for (var i = 0; i < cmd.params.times; i++) {
+        Reveal.next();
+    }
+}
+
+function cmdPrevious(cmd) {
+    for (var i = 0; i < cmd.params.times; i++) {
+        Reveal.prev();
+    }
+}
 
