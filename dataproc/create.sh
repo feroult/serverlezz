b37cb6250c31ddb1358b6d286ce69182869cf6d7
feroult <feroult@gmail.com>
2018-10-22 15:10:25 -0300

    demo

--- dataproc/create.sh
+++ dataproc/create.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 gcloud dataproc clusters create data-proc-fest \
-    --bucket net_src \
+    --bucket data-proc-fest \
     --num-workers 2 \
     --initialization-actions gs://data-proc-fest/scripts/init.sh
