a77b2171501cac85c773fe7947680cb2581e7aec
feroult <feroult@gmail.com>
2018-10-31 00:15:11 -0300

    dataproc

--- dataproc/scripts/create.sh
+++ dataproc/scripts/create.sh
@@ -3,5 +3,5 @@
 gcloud dataproc clusters create data-fest-demo \
     --zone=us-east1-b \
     --bucket data-fest-proc \
-    --num-workers 2 \
+    --num-workers 3 \
     --initialization-actions gs://data-fest-proc/scripts/init.sh
